5313a96c68b537f30f16d7062311dc581a7dd85a
Cordelia L <87583181+C3PO3@users.noreply.github.com>
2024-04-26 17:02:12 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const { MongoClient } = require('mongodb');
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -14,20 +13,6 @@ app.get('/process_stock.js', (req, res) => {
     // Handle form submission here
     const searchTerm = req.query.searchTerm;
     const searchType = req.query.searchType;
-
-    const uri = 'mongodb+srv://liagriffeon:zG2KLn57TAQ65IN6@stock.1k5sojy.mongodb.net';
-    const dbName = 'Stock';
-    const collectionName = 'PublicCompanies';
-    const client = new MongoClient(uri);
-
-    try {
-        client.connect();
-        
-    } catch (error) {
-        console.error('Error inserting data:', error);
-    } finally {
-        client.close();
-    }
 
     // You can perform further processing here, like fetching data from an API
     console.log('WORKING HERE');
