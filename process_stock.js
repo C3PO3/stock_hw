9f70ab3f1a688ecd319ecce6d2d7698323d652d9
Cordelia L <87583181+C3PO3@users.noreply.github.com>
2024-04-26 12:02:31 -0400

    Add files via upload

--- process_stock.js
+++ process_stock.js
@@ -1,12 +1,11 @@
-
 const express = require('express');
 const { MongoClient } = require('mongodb');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000; // Use dynamic port provided by Heroku or default to 3000
 
 // MongoDB connection URI
-const uri = 'mongodb+srv://liagriffeon:zG2KLn57TAQ65IN6@stock.1k5sojy.mongodb.net';
+const uri = 'mongodb+srv://username:password@host/database'; // Update with your MongoDB connection URI
 const dbName = 'Stock';
 const collectionName = 'PublicCompanies';
 
@@ -56,3 +55,62 @@ client.connect(err => {
 });
 
 module.exports = app;
+
+
+// const express = require('express');
+// const { MongoClient } = require('mongodb');
+
+// const app = express();
+// const port = 3000;
+
+// // MongoDB connection URI
+// const uri = 'mongodb+srv://liagriffeon:zG2KLn57TAQ65IN6@stock.1k5sojy.mongodb.net';
+// const dbName = 'Stock';
+// const collectionName = 'PublicCompanies';
+
+// // MongoDB client
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// client.connect(err => {
+//     if (err) {
+//         console.error('Error connecting to MongoDB:', err);
+//         return;
+//     }
+//     console.log('Connected to MongoDB');
+
+//     app.get('/process', async (req, res) => {
+//         try {
+//             const searchTerm = req.query.searchTerm;
+//             const searchType = req.query.searchType;
+
+//             const db = client.db(dbName);
+//             const collection = db.collection(collectionName);
+
+//             let query;
+//             if (searchType === 'ticker') {
+//                 query = { ticker: searchTerm };
+//             } else if (searchType === 'company') {
+//                 query = { company: searchTerm };
+//             } else {
+//                 res.status(400).send('Invalid search type');
+//                 return;
+//             }
+
+//             const result = await collection.find(query).toArray();
+
+//             console.log('Search Results:');
+//             console.log(result);
+
+//             res.send(result);
+//         } catch (error) {
+//             console.error('Error processing request:', error);
+//             res.status(500).send('Internal Server Error');
+//         }
+//     });
+
+//     app.listen(port, () => {
+//         console.log(`Server running at http://localhost:${port}`);
+//     });
+// });
+
+// module.exports = app;
