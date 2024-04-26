1a136b0ab270b756055853b03245a69c28a88af5
Cordelia L <87583181+C3PO3@users.noreply.github.com>
2024-04-26 17:53:55 -0400

    Add files via upload

--- process_stock.js
+++ process_stock.js
@@ -14,59 +14,109 @@ const client = new MongoClient(uri);
 
 console.log("AHHHHHHHHHHHH");
 
-client.connect(err => {
-    if (err) {
-        console.error('Error connecting to MongoDB:', err);
-        return;
-    }
-    console.log('Connected to MongoDB');
+async function startServer() {
+    try {
+        await client.connect();
+        console.log('Connected to MongoDB');
 
-    app.get('/process', async (req, res) => {
-        try {
-            const searchTerm = req.query.searchTerm;
-            const searchType = req.query.searchType;
+        app.get('/process', async (req, res) => {
+            try {
+                const searchTerm = req.query.searchTerm;
+                const searchType = req.query.searchType;
 
-            const db = client.db(dbName);
-            const collection = db.collection(collectionName);
+                const db = client.db(dbName);
+                const collection = db.collection(collectionName);
 
-            let query;
-            if (searchType === 'ticker') {
-                query = { ticker: searchTerm };
-            } else if (searchType === 'company') {
-                query = { company: searchTerm };
-            } else {
-                res.status(400).send('Invalid search type');
-                return;
+                let query;
+                if (searchType === 'ticker') {
+                    query = { ticker: searchTerm };
+                } else if (searchType === 'company') {
+                    query = { company: searchTerm };
+                } else {
+                    res.status(400).send('Invalid search type');
+                    return;
+                }
+
+                const result = await collection.find(query).toArray();
+
+                console.log('Search Results:');
+                console.log(result);
+
+                res.send(result);
+            } catch (error) {
+                console.error('Error processing request:', error);
+                res.status(500).send('Internal Server Error');
             }
+        });
 
-            const result = await collection.find(query).toArray();
+        app.listen(port, () => {
+            console.log(`Server running at http://localhost:${port}`);
+        });
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+    }
+}
 
-            console.log('Search Results:');
-            console.log(result);
+startServer();
 
-            res.send(result);
-        } catch (error) {
-            console.error('Error processing request:', error);
-            res.status(500).send('Internal Server Error');
-        }
-    });
+module.exports = app;
 
-    // app.listen(port, () => {
-    //     console.log(`Server running at http://localhost:${port}`);
-    // });
-});
+// const express = require('express');
+// const { MongoClient } = require('mongodb');
+
+// const app = express();
+// const port = process.env.PORT || 3000;
+
+// // MongoDB connection URI
+// const uri = 'mongodb+srv://liagriffeon:zG2KLn57TAQ65IN6@stock.1k5sojy.mongodb.net';
+// const dbName = 'Stock';
+// const collectionName = 'PublicCompanies';
+
+// // MongoDB client
+// const client = new MongoClient(uri);
+
+// console.log("AHHHHHHHHHHHH");
+
+// client.connect(err => {
+//     if (err) {
+//         console.error('Error connecting to MongoDB:', err);
+//         return;
+//     }
+//     console.log('Connected to MongoDB');
+
+//     app.get('/process', async (req, res) => {
+//         try {
+//             const searchTerm = req.query.searchTerm;
+//             const searchType = req.query.searchType;
+
+//             const db = client.db(dbName);
+//             const collection = db.collection(collectionName);
+
+//             let query;
+//             if (searchType === 'ticker') {
+//                 query = { ticker: searchTerm };
+//             } else if (searchType === 'company') {
+//                 query = { company: searchTerm };
+//             } else {
+//                 res.status(400).send('Invalid search type');
+//                 return;
+//             }
+
+//             const result = await collection.find(query).toArray();
+
+//             console.log('Search Results:');
+//             console.log(result);
+
+//             res.send(result);
+//         } catch (error) {
+//             console.error('Error processing request:', error);
+//             res.status(500).send('Internal Server Error');
+//         }
+//     });
+
+//     // app.listen(port, () => {
+//     //     console.log(`Server running at http://localhost:${port}`);
+//     // });
+// });
 
 // module.exports = app;
-
-// async function main() {
-//     try {
-//         // Read data from CSV file
-//         const filename = 'companies-1.csv';
-//         const data = await readDataFromFile(filename);
-
-//         // Insert data into MongoDB
-//         await insertDataIntoDB(data);
-//     } catch (error) {
-//         console.error('Error:', error);
-//     }
-// }
