f8f691d6dabdb9c0e56708d39d0a5fcfe04fd357
Cordelia L <87583181+C3PO3@users.noreply.github.com>
2024-04-26 18:10:32 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.get('/', (req, res) => {
 //     res.send(result);
 // });
 
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-//     console.log(`Server is running on port ${PORT}`);
-// });
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
